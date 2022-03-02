df7bd817dbbf9752b69a885395d0d9e81be98815
Terry Xu <knight3001@gmail.com>
2022-03-02 17:16:04 +1000

    LaunchProfile

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 
 import LaunchList from "./components/LaunchList";
+import LaunchProfile from "./components/LaunchProfile";
 
 function App() {
   return (
     <div className="App">
       <LaunchList />
+      <LaunchProfile />
     </div>
   );
 }
--- src/components/LaunchProfile/index.tsx
+++ src/components/LaunchProfile/index.tsx
@@ -1,5 +1,31 @@
-import React, { useState, useContext, useEffect, useCallback, useMemo } from 'react';
+import React, {
+  useState,
+  useContext,
+  useEffect,
+  useCallback,
+  useMemo,
+} from "react";
+import { useLaunchProfileQuery } from "../../generated/graphql";
+import LaunchProfile from "./LaunchProfile";
 
-const LaunchProfileContainer = () => {}
+const LaunchProfileContainer = () => {
+  const { data, error, loading } = useLaunchProfileQuery({
+    variables: { id: "42" },
+  });
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>ERROR</div>;
+  }
+
+  if (!data) {
+    return <div>Select a flight from the panel</div>;
+  }
+
+  return <LaunchProfile data={data} />;
+};
 
 export default LaunchProfileContainer;
